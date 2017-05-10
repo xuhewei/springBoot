58be299e863fff310aa4c3de3adaf3e6f036d1a0
xhw1990514 <1037901372@q.com>
2017-05-10 08:12:43 +0800

    初始化

---
+++ src/main/java/cn/xu/springboot/controller/UserController.java
@@ -0,0 +1,29 @@
+package cn.xu.springboot.controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.xu.springboot.entity.User;
+
+@EnableAutoConfiguration
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/{id}")
+    public User view(@PathVariable("id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("zhang");
+        return user;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserController.class);
+    }
+
+}
